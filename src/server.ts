866164f7afa2c5bdc3bc7625708c8d4e83cc128b
jobayer-hossen <emon45088@gmail.com>
2025-07-04 11:22:16 +0600

    cors error handeling

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,14 @@ import { Server } from "http";
 import app from "./app";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 import { connectDB } from "./app/config/mongoDBDatabase";
 dotenv.config();
 
 const PORT = 4000;
 let server: Server;
+
+app.use(cors({ origin: "*" })); 
 
 connectDB().then(() => {
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
